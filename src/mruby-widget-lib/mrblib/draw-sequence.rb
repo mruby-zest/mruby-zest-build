fbc045a76bdd00a0c8343c68b6fbad5467ef4637
fundamental <mark.d.mccurry@gmail.com>
2016-05-10 11:20:00 -0400

    Refactor Draw Sequence Support

--- src/mruby-widget-lib/mrblib/draw-sequence.rb
+++ src/mruby-widget-lib/mrblib/draw-sequence.rb
@@ -9,3 +9,113 @@ class DrawSeqNode
     end
 end
 
+class DrawSequence
+    attr_accessor :window, :damage, :seq
+    def initialize
+        @damage   = []
+        @seq      = []
+    end
+
+    # Force a redraw in the given region
+    def damage_region(region, layer)
+        if(@damage != :everything)
+            @damage << [region, layer]
+        end
+        @window.refresh
+    end
+
+    # Add a widget to the draw sequence
+    def add(item, xoff, yoff)
+        if(item.respond_to? :draw)
+            @seq << DrawSeqNode.new(xoff,yoff,item.w,item.h,item)
+        end
+    end
+
+    def make_draw_sequence_recur(item, xoff, yoff)
+        if(item && xoff && yoff)
+            add(item, xoff, yoff)
+            item.children.each do |ch|
+                if(ch.y && ch.x)
+                    make_draw_sequence_recur(ch, xoff+ch.x, yoff+ch.y)
+                else
+                    puts "bad x/y"
+                    puts "widget = <#{ch}>"
+                end
+            end
+        end
+    end
+
+    def make_draw_sequence(root)
+        @seq = []
+        make_draw_sequence_recur(root, 0, 0)
+    end
+
+    #Run the full draw sequence
+    def render(vg, w, h, fbo)
+        (background_fbo, animation_fbo, overlay_fbo, redraw_fbo) = fbo
+        puts background_fbo
+        puts animation_fbo
+        puts overlay_fbo
+        puts redraw_fbo
+        #[@background_fbo, @animation_fbo, @overlay_fbo, @redraw_fbo].each do |fbo|
+        #    fbo.select
+        #    GL::gl_viewport(0, 0, w, h);
+        #    GL::gl_clear_color(0, 0, 0, 1.0);
+        #    GL::gl_clear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+        #end
+        redraw_fbo.select
+        #puts "running draw sequence on #{w}x#{h} window"
+        GL::gl_viewport(0, 0, w, h);
+        GL::gl_clear_color(0, 0, 0, 1.0);
+        GL::gl_clear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+
+        GL::gl_viewport(0, 0, w, h);
+        vg.draw(w,h,1.0) do |v|
+            @seq.each do |n|
+                v.spork do |vv|
+                    vv.translate(n.x,n.y)
+                    n.item.draw(vv)
+                end
+            end
+        end
+        redraw_fbo.deselect
+
+        #puts @damage
+        @damage.each do |dmg|
+            d = dmg[0]
+            GL::gl_viewport(0,0,w,h)
+            GL::gl_scissor(d.x, h-(d.y+d.h), d.w, d.h)
+            GL::gl_clear_color(0, 0, 0, 1.0)
+            GL::gl_clear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+            GL::gl_scissor_end
+        end
+
+
+        redraw_img = get_image(redraw_fbo)
+        #@damage = [[Rect.new(0, 0, 512,512)]]
+
+        GL::gl_viewport(0, 0, w, h);
+        vg.draw(w, h, 1.0) do |v|
+            @damage.each do |dmg|
+                d = dmg[0]
+                pat = vg.image_pattern(0,h,w,h,0,redraw_img.image, 1.0)
+                vg.path do |p|
+                    p.rect(d.x,d.y,d.w,d.h)
+                    v.fill_paint pat
+                    v.fill
+                end
+            end
+        end
+
+        @damage = []
+    end
+
+    def get_image(fbo)
+        im = fbo.image $vg
+        Nanovg::ImageHandle.new($vg, im)
+    end
+
+
+    private :make_draw_sequence_recur
+
+end
--- src/mruby-widget-lib/mrblib/script.rb
+++ src/mruby-widget-lib/mrblib/script.rb
@@ -6,11 +6,11 @@
 
 class ZRunner
     def initialize
-        @events  = UiEventSeq.new
-        @mx      = 0
-        @my      = 0
-        @clicked = nil
-        @damage  = []
+        @events   = UiEventSeq.new
+        @draw_seq = DrawSequence.new
+        @mx       = 0
+        @my       = 0
+        @clicked  = nil
 
         #Framebuffers
         @background_fbo = nil
@@ -24,91 +24,93 @@ class ZRunner
         @redraw_img     = nil
     end
 
-    def draw_damage(region, layer)
-        if(@damage != :everything)
-            @damage << [region, layer]
+    ########################################
+    #       Graphics Init Routines         #
+    ########################################
+
+    def setup
+        puts "setup..."
+        if(!@widget)
+            puts "No Widget was allocated"
+            puts "This is typically a problem with running the code from the wrong subdirectory"
+            puts "If mruby-zest cannot find the qml source files, then the UI cannot be created"
+            raise "Impossible Widget"
         end
-        @window.refresh
-    end
 
-    def draw_seq_add(item, xoff, yoff)
-        if(item.respond_to? :draw)
-            @draw_seq << DrawSeqNode.new(xoff,yoff,item.w,item.h,item)
+
+        #Setup OpenGL Interface
+        init_pugl
+
+        if(@widget.label)
+            @window.title = @widget.label
         end
-    end
 
-    def make_draw_sequence_recur(item, xoff, yoff)
-        if(item && xoff && yoff)
-            draw_seq_add(item, xoff, yoff)
-            item.children.each do |ch|
-                if(ch.y && ch.x)
-                    make_draw_sequence_recur(ch, xoff+ch.x, yoff+ch.y)
-                else
-                    puts "bad x/y"
-                    puts "widget = <#{ch}>"
-                end
-            end
-        end
-    end
-
-    def make_draw_sequence(root)
-        @draw_seq = []
-        make_draw_sequence_recur(root, 0, 0)
-    end
-
-    def run_draw_sequence(vg,w,h)
-        #[@background_fbo, @animation_fbo, @overlay_fbo, @redraw_fbo].each do |fbo|
-        #    fbo.select
-        #    GL::gl_viewport(0, 0, w, h);
-        #    GL::gl_clear_color(0, 0, 0, 1.0);
-        #    GL::gl_clear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+        #Initial sizing
+        #if(@widget.w && @widget.h)
+        #    #resize(@widget.w, @widget.h)
+        #    #@window.size = [@widget.w, @widget.h]
+        #else
+            @widget.w,@widget.h = [512, 512]#@window.size
         #end
-        @redraw_fbo.select
-        #puts "running draw sequence on #{w}x#{h} window"
-        GL::gl_viewport(0, 0, w, h);
-        GL::gl_clear_color(0, 0, 0, 1.0);
-        GL::gl_clear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 
-        GL::gl_viewport(0, 0, w, h);
-        vg.draw(w,h,1.0) do |v|
-            @draw_seq.each do |n|
-                v.spork do |vv|
-                    vv.translate(n.x,n.y)
-                    n.item.draw(vv)
-                end
-            end
-        end
-        @redraw_fbo.deselect
+        doSetup(nil, @widget)
 
-        #puts @damage
-        @damage.each do |dmg|
-            d = dmg[0]
-            GL::gl_viewport(0,0,w,h)
-            GL::gl_scissor(d.x, @h-(d.y+d.h), d.w, d.h)
-            GL::gl_clear_color(0, 0, 0, 1.0)
-            GL::gl_clear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-            GL::gl_scissor_end
-        end
+        perform_layout
+        @draw_seq.make_draw_sequence(@widget)
 
-
-        @redraw_img = get_image(@redraw_fbo)
-        #@damage = [[Rect.new(0, 0, 512,512)]]
-
-        GL::gl_viewport(0, 0, w, h);
-        vg.draw(w, h, 1.0) do |v|
-            @damage.each do |dmg|
-                d = dmg[0]
-                pat = vg.image_pattern(0,h,w,h,0,@redraw_img.image, 1.0)
-                vg.path do |p|
-                    p.rect(d.x,d.y,d.w,d.h)
-                    v.fill_paint pat
-                    v.fill
-                end
-            end
-        end
-
-        @damage = []
+        @widget.db.make_rdepends
+        puts "Widget setup..."
+        #puts @widget.db
     end
+
+    def init_pugl
+        puts "init pugl"
+        @window = GL::PUGL.new self
+        @draw_seq.window = @window
+        init_gl
+        @window.impl = self
+    end
+
+    def init_gl
+        puts "init gl"
+        @window.make_current
+        @window.size = [512,512]
+        @w,@h=*@window.size
+        @w = 512
+        @h = 512
+
+        @vg     = NVG::Context.new(NVG::ANTIALIAS | NVG::STENCIL_STROKES | NVG::DEBUG)
+
+        #Global Initialize
+        $vg     = @vg
+        $remote = OSC::Remote.new
+
+        #Load Fonts
+        sans = `find . -type f | grep Regular.ttf`.split[0]
+        @vg.create_font('sans', sans)
+        bold = `find . -type f | grep Bold.ttf`.split[0]
+        @vg.create_font('bold', bold)
+
+        #Load Overlay image
+        @backdrop = @vg.create_image('../template.png', 0)
+        puts "window width=#{@w}"
+        puts "window height=#{@h}"
+
+        build_fbo
+
+    end
+
+    def build_fbo
+        @background_fbo = GL::FBO.new(@w, @h)
+        @animation_fbo  = GL::FBO.new(@w, @h)
+        @overlay_fbo    = GL::FBO.new(@w, @h)
+        @redraw_fbo     = GL::FBO.new(@w, @h)
+    end
+
+
+    ########################################
+    #            Event Handling            #
+    ########################################
 
     #holds true only in cases of a spacial partitioning
     def activeWidget(mx=@mx, my=@my, xoff=0,yoff=0,scope=@widget)
@@ -157,6 +159,71 @@ class ZRunner
         end
     end
 
+    def quit
+        @keep_running = false
+    end
+
+    def resize(w,h)
+        @events.record([:windowResize, {:w => w, :h => h}])
+    end
+
+    def cursor(x,y)
+        @events.record([:mouseMove, {:x => x, :y => y}])
+    end
+
+    def mouse(button, action, x, y)
+        mod = nil
+        if(action == 1)
+            @events.record([:mousePress,   {:button => button, :action => action, :mod => mod}])
+        else
+            @events.record([:mouseRelease, {:button => button, :action => action, :mod => mod}])
+        end
+    end
+
+    def load_event_seq
+        @events.reload File.open("/tmp/zest-event-log.txt", "r")
+    end
+
+    def handle_events
+        cnt = 0
+        @events.ev.each do |ev|
+            cnt += 1
+            #puts "handling #{ev}"
+            if(ev[0] == :mousePress)
+                mouse = MouseButton.new(ev[1][:button], Pos.new(@mx, @my))
+                handleMousePress(mouse)
+            elsif(ev[0] == :mouseRelease)
+                mouse = MouseButton.new(ev[1][:button], Pos.new(@mx, @my))
+                handleMouseRelease(mouse)
+            elsif(ev[0] == :mouseMove)
+                handleCursorPos(ev[1][:x],ev[1][:y])
+            elsif(ev[0] == :windowResize)
+                @events.ignore
+                @window.size = [ev[1][:w], ev[1][:h]]
+                puts "doing a resize to #{[ev[1][:w], ev[1][:h]]}"
+
+                @w = @widget.w  = ev[1][:w]
+                @h = @widget.h  = ev[1][:h]
+
+                #Layout Widgets again
+                perform_layout
+                #Build Draw order
+                @draw_seq.make_draw_sequence(@widget)
+                #Reset textures
+                build_fbo
+
+                @draw_seq.damage_region(Rect.new(0,0,@widget.w, @widget.h), 0)
+            end
+        end
+
+        @events.next_frame
+        cnt
+    end
+
+    ########################################
+    #      Widget Hotloading Support       #
+    ########################################
+
     #Setup widget graph
     def doSetup(wOld, wNew)
         if(wNew.respond_to? :onSetup)
@@ -184,102 +251,6 @@ class ZRunner
         end
     end
 
-    def quit
-        @keep_running = false
-    end
-
-    def resize(w,h)
-        @events.record([:windowResize, {:w => w, :h => h}])
-    end
-
-    def cursor(x,y)
-        @events.record([:mouseMove, {:x => x, :y => y}])
-    end
-
-    def mouse(button, action, x, y)
-        mod = nil
-        if(action == 1)
-            @events.record([:mousePress,   {:button => button, :action => action, :mod => mod}])
-        else
-            @events.record([:mouseRelease, {:button => button, :action => action, :mod => mod}])
-        end
-    end
-
-    def init_glfw_callbacks
-        puts "init glfw"
-        @window.set_mouse_button_callback do |mouse, button, action, mod|
-            if(action == 1)
-                @events.record([:mousePress,   {:button => button, :action => action, :mod => mod}])
-            else
-                @events.record([:mouseRelease, {:button => button, :action => action, :mod => mod}])
-            end
-        end
-
-        @window.set_cursor_pos_callback do |c, x, y|
-            @events.record([:mouseMove, {:x => x, :y => y}])
-        end
-
-        @window.set_key_callback do |key, a, b, c, d|
-            @events.record([:keyPress, {:key => a, :down => c}])
-        end
-
-        @window.set_size_callback do |key, w, h|
-            @events.record([:windowResize, {:w => w, :h => h}])
-        end
-    end
-
-    def init_pugl
-        puts "init pugl"
-        @window = GL::PUGL.new self
-        init_gl
-        @window.impl = self
-    end
-
-    def init_gl
-        puts "init gl"
-        @window.make_current
-        @window.size = [512,512]
-        @w,@h=*@window.size
-        @w = 512
-        @h = 512
-
-        @vg     = NVG::Context.new(NVG::ANTIALIAS | NVG::STENCIL_STROKES | NVG::DEBUG)
-
-        #Global Initialize
-        $vg     = @vg
-        $remote = OSC::Remote.new
-
-        #Load Fonts
-        sans = `find . -type f | grep Regular.ttf`.split[0]
-        @vg.create_font('sans', sans)
-        bold = `find . -type f | grep Bold.ttf`.split[0]
-        @vg.create_font('bold', bold)
-
-        #Load Overlay image
-        @backdrop = @vg.create_image('../template.png', 0)
-        puts "window width=#{@w}"
-        puts "window height=#{@h}"
-
-        build_fbo
-
-    end
-
-    def build_fbo
-        @background_fbo = GL::FBO.new(@w, @h)
-        @animation_fbo  = GL::FBO.new(@w, @h)
-        @overlay_fbo    = GL::FBO.new(@w, @h)
-        @redraw_fbo     = GL::FBO.new(@w, @h)
-        #@background_img = get_image(@background_fbo)
-        #@animation_img  = get_image(@animation_fbo)
-        #@overlay_img    = get_image(@overlay_fbo)
-        #@redraw_img     = get_image(@redraw_fbo)
-    end
-
-    def get_image(fbo)
-        im = fbo.image $vg
-        Nanovg::ImageHandle.new($vg, im)
-    end
-
     def draw
         #Setup Profilers
         p_total = TimeProfile.new
@@ -292,21 +263,15 @@ class ZRunner
 
         w = @window.w
         h = @window.h
-        #puts "window.w = <#{w}>"
+
+        fbo = [@background_fbo, @animation_fbo, @overlay_fbo, @redraw_fbo]
 
         #Draw the widget tree
         p_draw.time do
-            run_draw_sequence(@vg, w, h)
+            @draw_seq.render(@vg, w, h, fbo)
         end
 
-        #Draw Overlay
-        #draw_overlay(w,h,frames)
-
         p_total.stop
-
-        #puts ObjectSpace.count_objects.keys
-        #puts ObjectSpace.count_objects.values
-        #puts "#{frames}, #{p_total.avg_hz.to_i}Hz, #{p_total.avg}, #{p_draw.avg}, #{p_code.avg}, #{1000.0/60.0}"
     end
 
 
@@ -366,84 +331,6 @@ class ZRunner
         end
     end
 
-    def load_event_seq
-        @events.reload File.open("/tmp/zest-event-log.txt", "r")
-    end
-
-    $resize_count = 0
-    def handle_events
-        cnt = 0
-        @events.ev.each do |ev|
-            cnt += 1
-            #puts "handling #{ev}"
-            if(ev[0] == :mousePress)
-                mouse = MouseButton.new(ev[1][:button], Pos.new(@mx, @my))
-                handleMousePress(mouse)
-            elsif(ev[0] == :mouseRelease)
-                mouse = MouseButton.new(ev[1][:button], Pos.new(@mx, @my))
-                handleMouseRelease(mouse)
-            elsif(ev[0] == :mouseMove)
-                handleCursorPos(ev[1][:x],ev[1][:y])
-            elsif(ev[0] == :windowResize)
-                $resize_count += 1
-                #if($resize_count < 10)
-                @events.ignore
-                @window.size = [ev[1][:w], ev[1][:h]]
-                puts "doing a resize to #{[ev[1][:w], ev[1][:h]]}"
-
-                @w = @widget.w  = ev[1][:w]
-                @h = @widget.h  = ev[1][:h]
-
-                #Layout Widgets again
-                perform_layout
-                #Build Draw order
-                make_draw_sequence(@widget)
-                #Reset textures
-                build_fbo
-
-                draw_damage(Rect.new(0,0,@widget.w, @widget.h), 0)
-            end
-        end
-
-        @events.next_frame
-        cnt
-    end
-
-    def setup
-        puts "setup..."
-        if(!@widget)
-            puts "No Widget was allocated"
-            puts "This is typically a problem with running the code from the wrong subdirectory"
-            puts "If mruby-zest cannot find the qml source files, then the UI cannot be created"
-            raise "Impossible Widget"
-        end
-
-
-        #Setup OpenGL Interface
-        init_pugl
-
-        if(@widget.label)
-            @window.title = @widget.label
-        end
-
-        #Initial sizing
-        #if(@widget.w && @widget.h)
-        #    #resize(@widget.w, @widget.h)
-        #    #@window.size = [@widget.w, @widget.h]
-        #else
-            @widget.w,@widget.h = [512, 512]#@window.size
-        #end
-
-        doSetup(nil, @widget)
-
-        perform_layout
-        make_draw_sequence(@widget)
-
-        @widget.db.make_rdepends
-        puts "Widget setup..."
-        #puts @widget.db
-    end
-
     def doRun(&block)
         @widget = block.call
         if(@widget.nil?)
@@ -464,7 +351,7 @@ class ZRunner
         print '..'
 
         #Do initial draw
-        draw_damage(Rect.new(0, 0, @w, @h), 0)
+        @draw_seq.damage_region(Rect.new(0, 0, @w, @h), 0)
 
         frames = 0
         while(frames < 100000 && @window != nil && @keep_running)
@@ -502,20 +389,12 @@ class ZRunner
                 end
                 t_setup = Time.new
 
-                #sizeChange = @window.size != [@widget.w, @widget.h]
-                #locsChange = nwidget || sizeChange
-
-                ##Update main window size
-                #if(sizeChange)
-                #    @widget.w, @widget.h  = @window.size
-                #end
-
                 #Layout Widgets again
                 #Build Draw order
                 t_layout_before = Time.new
                 if(nwidget)
                     perform_layout
-                    make_draw_sequence(@widget)
+                    @draw_seq.make_draw_sequence(@widget)
                 end
                 t_layout_after = Time.new
 
@@ -533,10 +412,6 @@ class ZRunner
             end
 
             p_total.stop
-
-            #puts ObjectSpace.count_objects.keys
-            #puts ObjectSpace.count_objects.values
-            #puts "#{frames}, #{p_total.avg_hz.to_i}Hz, #{p_total.avg}, #{p_draw.avg}, #{p_code.avg}, #{1000.0/60.0}"
         end
 
         if(@window.should_close || true)
