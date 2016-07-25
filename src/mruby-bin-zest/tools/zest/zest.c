736a740665d322a03c4f28da20e4bb6610a68cea
fundamental <mark.d.mccurry@gmail.com>
2016-07-25 17:51:44 -0400

    Shift --no-hotload Flag To --hotload Flag

--- src/mruby-bin-zest/tools/zest/zest.c
+++ src/mruby-bin-zest/tools/zest/zest.c
@@ -16,10 +16,10 @@ parse_arguments(int argc, char **argv)
 {
     cmd_arg.remote_address   = "localhost:1337";
     cmd_arg.widget_selection = "MainWindow";
-    cmd_arg.hotload          = 1;
+    cmd_arg.hotload          = 0;
     for(int i=0; i<argc; ++i)
-        if(!strcmp("--no-hotload", argv[i]))
-            cmd_arg.hotload = 0;
+        if(!strcmp("--hotload", argv[i]))
+            cmd_arg.hotload = 1;
 
     (void) argc;
     (void) argv;
