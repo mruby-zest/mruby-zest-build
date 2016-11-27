897504fd6431405ee274bc67d017629bc31d6280
fundamental <mark.d.mccurry@gmail.com>
2016-11-27 11:25:26 -0500

    Update Linux Package Script

--- linux-pack.sh
+++ linux-pack.sh
@@ -3,10 +3,10 @@
 rm -r /opt/zyn-fusion
 mkdir /opt/zyn-fusion
 chown mark:users /opt/zyn-fusion
-echo "3.0.0 Pre-Release " >> /opt/zyn-fusion/VERSION
-echo "Build on"           >> /opt/zyn-fusion/VERSION
-echo `date`               >> /opt/zyn-fusion/VERSION
-cp   -a /usr/local/lib/lv2/ZynAddSubFX.lv2presets /opt/zyn-fusion/
+echo "Version 3.0.0" >> /opt/zyn-fusion/VERSION
+echo "Build on"      >> /opt/zyn-fusion/VERSION
+echo `date`          >> /opt/zyn-fusion/VERSION
+cp   -a /usr/lib/lv2/ZynAddSubFX.lv2presets       /opt/zyn-fusion/
 cp   -a /home/mark/zynaddsubfx/instruments/banks  /opt/zyn-fusion/
 cp      ./package/glpsol                          /opt/zyn-fusion/
 cp      ./package/libzest.so                      /opt/zyn-fusion/
@@ -19,7 +19,12 @@ mkdir   /opt/zyn-fusion/ZynAddSubFX.lv2
 cp      /home/mark/zynaddsubfx/build/src/Plugin/ZynAddSubFX/lv2/* /opt/zyn-fusion/ZynAddSubFX.lv2/
 cp      /home/mark/zynaddsubfx/build/src/Plugin/ZynAddSubFX/vst/ZynAddSubFX.so /opt/zyn-fusion/
 cp      /home/mark/zynaddsubfx/build/src/zynaddsubfx /opt/zyn-fusion/
+cp      ./install-linux.sh /opt/zyn-fusion/
+cp      ./package-README.txt /opt/zyn-fusion/README.txt
+cp      ./zyn-fusion-ELUA.txt /opt/zyn-fusion/
+cp      /home/mark/zynaddsubfx/COPYING /opt/zyn-fusion/COPYING.zynaddsubfx
+cp      ./deps/glpk-4.52/COPYING       /opt/zyn-fusion/COPYING.glpsol
 cd /opt/
-rm -f zyn-fusion-3.0.0pre.tar zyn-fusion-3.0.0pre.tar.bz2
-tar cf zyn-fusion-3.0.0pre.tar ./zyn-fusion
-bzip2 zyn-fusion-3.0.0pre.tar
+rm -f zyn-fusion-3.0.0.tar zyn-fusion-3.0.0.tar.bz2
+tar cf zyn-fusion-3.0.0.tar ./zyn-fusion
+bzip2 zyn-fusion-3.0.0.tar
