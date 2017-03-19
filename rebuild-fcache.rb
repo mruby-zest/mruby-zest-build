c9ccc09ad93f38894ade5384128079afd77c4b0b
fundamental <mark.d.mccurry@gmail.com>
2017-03-19 19:18:06 -0400

    Add Script to Rebuild Precompiled QML File Cache

---
+++ rebuild-fcache.rb
@@ -0,0 +1,15 @@
+require 'set'
+require_relative 'src/mruby-qml-parse/mrblib/main.rb'
+require_relative 'src/mruby-qml-parse/mrblib/parse-types.rb'
+require_relative 'src/mruby-qml-parse/mrblib/parser.rb'
+require_relative 'src/mruby-qml-parse/mrblib/prog-ir.rb'
+require_relative 'src/mruby-qml-parse/mrblib/prog-vm.rb'
+require_relative 'src/mruby-qml-parse/mrblib/react-attr.rb'
+require_relative 'src/mruby-qml-spawn/mrblib/build.rb'
+require_relative 'src/mruby-qml-spawn/mrblib/database.rb'
+require_relative 'src/mruby-qml-spawn/mrblib/loader.rb'
+
+ir = loadIR(nil)
+puts "length(IR) = #{ir.length}"
+$ruby_mode = :CRuby
+QmlIrToRuby.new(ir, nil, "testing-cache.rb")
