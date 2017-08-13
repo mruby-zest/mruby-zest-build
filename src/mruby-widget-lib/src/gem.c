63d617f93f6a588242d75d842957fecf4df59c11
fundamental <mark.d.mccurry@gmail.com>
2017-08-13 19:04:27 -0400

    Add "si" Remote#action() handler
    Fixes array based copy/paste

--- src/mruby-widget-lib/src/gem.c
+++ src/mruby-widget-lib/src/gem.c
@@ -741,6 +741,13 @@ mrb_remote_action(mrb_state *mrb, mrb_value self)
             args[1].s = arg;
             br_action(data->br, path, "is", args);
             free(arg);
+        } else if(argv[1].tt == MRB_TT_STRING && argv[2].tt == MRB_TT_FIXNUM) {
+            char *arg = strdup(mrb_string_value_ptr(mrb, argv[1]));
+            rtosc_arg_t args[2];
+            args[0].s = arg;
+            args[1].i = argv[2].value.i;
+            br_action(data->br, path, "si", args);
+            free(arg);
         } else if(argv[1].tt == MRB_TT_FIXNUM && argv[2].tt == MRB_TT_FIXNUM) {
             rtosc_arg_t args[2];
             args[0].i = argv[1].value.i;
